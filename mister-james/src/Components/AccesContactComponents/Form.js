4dff0aaae14c0aa6d60b47abd5b74bbd7a444c6c
yaniiish <yanis.herda14@gmail.com>
2023-02-08 16:42:11 +0100

    Form Ok
    a faire les verif de champs empty

--- mister-james/src/Components/AccesContactComponents/Form.js
+++ mister-james/src/Components/AccesContactComponents/Form.js
@@ -1,21 +1,43 @@
 import React from 'react'
-import { useEffect } from 'react';
+import { useRef, useState } from 'react';
 import { useForm } from "react-hook-form";
+import emailjs from '@emailjs/browser';
+
 
 
 function Form() {
+    
+    const form = useRef();
 
-    const {register, handleSubmit, formState: {errors, isSubmitSuccessful}, reset} = useForm()
+    const sendEmail = (e) => {
+      e.preventDefault();
 
-    console.log(isSubmitSuccessful);
 
+  
+      emailjs.sendForm('service_9q0lgbf', 'template_v96mtsl', form.current, 'XHVwM7FsPNcbLQ1ED')
+        .then((result) => {
+            console.log(result.text);
+            setStatus("Message envoyé !")
+            e.target.reset()
+        }, (error) => {
+            console.log(error.text);
 
-    const onSubmit = data => {
-        console.log(data);
+        });
+    };
+
+    const  [status, setStatus] = useState("");
+
+    const  renderAlert = () => {
+        <div>
+            <p>Je suis un test</p>
+        </div>
     }
 
+
+    
+
   return (
-    <form className='text-center mt-10' onSubmit={handleSubmit(onSubmit)}>
+    <form className='text-center mt-10' ref={form} onSubmit={sendEmail}>
         <h2 className='mb-10'>Ecris-moi</h2>
         <div className='global flex flex-col gap-8'>
             <div className='flex flex-col gap-8 lg:flex lg:flex-row lg:gap-4 mx-auto'>
@@ -23,40 +45,33 @@ function Form() {
                     <div className='self-start '>
                         <label className='underline text-lg font-semibold opacity-[80%]'>Nom </label>
                     </div>
-                    <input  {...register('nom', {required: '*Champs obligatoire'})} type='text' className='w-[300px] bg-dore border-2 focus:outline-none border-card rounded py-2 px-3'></input>
-                    <span className=''>{errors.nom?.message}</span>
+                    <input  type='text' className='w-[300px] bg-dore border-2 focus:outline-none border-card rounded py-2 px-3'></input>
                 </div>
                 <div className='flex w-fit flex-col items-center mx-auto'>
                     <div className='self-start '>
                         <label className='underline text-lg font-semibold opacity-[80%]'>Prénom </label>
                     </div>
-                    <input {...register('prenom', {required: '*Champs obligatoire'})} type='text' className='w-[300px] bg-dore border-2 focus:outline-none border-card rounded py-2 px-3'></input>
-                    <span className=''>{errors.prenom?.message}</span>
-
+                    <input  type='text' className='w-[300px] bg-dore border-2 focus:outline-none border-card rounded py-2 px-3'></input>
                 </div>
             </div>
             <div className='flex w-fit flex-col items-center mx-auto'>
                 <div className='self-start '>
                     <label className='underline text-lg font-semibold opacity-[80%]'>E-mail </label>
                 </div>
-                <input {...register('mail', {required: '*Champs obligatoire'})} type='email' className='w-[300px] lg:w-[610px] bg-dore border-2 focus:outline-none border-card rounded py-2 px-3'></input>
-                <span className=''>{errors.mail?.message}</span>
-
+                <input  type='email' className='w-[300px] lg:w-[610px] bg-dore border-2 focus:outline-none border-card rounded py-2 px-3'></input>
             </div>
             <div className='flex w-fit flex-col items-center mx-auto'>
                 <div className='self-start '>
                     <label className='underline text-lg font-semibold opacity-[80%]'>Message </label>
                 </div>
-                <textarea {...register('message', {required: '*Champs obligatoire'})} className='w-[300px] lg:w-[610px] bg-dore border-2 focus:outline-none border-card rounded py-2 px-3'></textarea>
-                <span className=''>{errors.message?.message}</span>
-
+                <textarea  className='w-[300px] lg:w-[610px] bg-dore border-2 focus:outline-none border-card rounded py-2 px-3'></textarea>
             </div>
-            {!isSubmitSuccessful? 
-                        <button type='submit' className='bg-noir w-fit mx-auto text-white py-2 px-8 rounded'>Envoyer</button>
-                        : 'Message envoyé'}
+                    <button type='submit' className='bg-noir w-fit mx-auto text-white py-2 px-8 rounded'>Envoyer</button>
+                    {status}
         </div>
     </form>
 
+   
 
   )
 }
