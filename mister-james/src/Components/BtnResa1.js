aea7e47d706f5fd384eb14c614f42103ac5ac1e2
yaniiish <yanis.herda14@gmail.com>
2023-02-28 09:01:37 +0100

    s

--- mister-james/src/Components/BtnResa1.js
+++ mister-james/src/Components/BtnResa1.js
@@ -6,11 +6,11 @@ function BtnResa1(props) {
   return (
     // on utilise les props pour avoir un bg diffrent dans les 2 menu navbar
       <PopupButton 
-      className={`${props.background} ${props.px} ${props.py} px-8 py-2 text-white uppercase font-black rounded	drop-shadow-2xl tracking-widest hover:opacity-80 stroke-noir`}
+      className={`${props.background} ${props.px} ${props.py} px-8 py-2 text-white uppercase font-black rounded	drop-shadow-2xl 
+      tracking-widest hover:opacity-80 stroke-noir`}
       url="https://calendly.com/test-calendlyy/prise-rdv"
       rootElement={document.getElementById("root")}
-      text='Réserver'
-      
+      text='Réserver'     
         />
   )
 }
